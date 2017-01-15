d76ec2ed03ae7f7ba9461ca01e7795e2f28e1dcc
Mahavir Jain <mahavirpj@gmail.com>
2017-01-15 14:13:45 +0530

    misc: commit missing files in previous timer support
    Signed-off-by: Mahavir Jain <mahavirpj@gmail.com>

---
+++ include/kernel/helper.h
@@ -0,0 +1,16 @@
+#ifndef __HELPER_H__
+#define __HELPER_H__
+
+static inline unsigned char inportb (unsigned short _port)
+{
+    unsigned char rv;
+    __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
+    return rv;
+}
+
+static inline void outportb (unsigned short _port, unsigned char _data)
+{
+    __asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
+}
+
+#endif /* __HELPER_H__ */
---
+++ kernel/timer.c
@@ -0,0 +1,38 @@
+#include <isr.h>
+#include <helper.h>
+
+/* This will keep track of how many ticks that the system
+ *  has been running for */
+static volatile int timer_ticks;
+
+static void timer_handler(registers_t *r)
+{
+	(void) r;
+	/* Increment our 'tick count' */
+	timer_ticks++;
+}
+
+static void timer_phase(int hz)
+{
+	int divisor = 1193180 / hz;       /* Calculate our divisor */
+	outportb(0x43, 0x36);             /* Set our command byte 0x36 */
+	outportb(0x40, divisor & 0xFF);   /* Set low byte of divisor */
+	outportb(0x40, divisor >> 8);     /* Set high byte of divisor */
+}
+
+/* Sets up the system clock by installing the timer handler
+ *  into IRQ0 */
+void init_timer(int freq)
+{
+	timer_phase(freq);
+	/* Installs 'timer_handler' to IRQ0 */
+	irq_install_handler(0, timer_handler);
+}
+
+void wait_ms(int ms)
+{
+	volatile int start = timer_ticks;
+	start += (ms / 10);
+	while (start > timer_ticks)
+		;
+}
